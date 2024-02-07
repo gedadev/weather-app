01be8a8db652ba1ae3fc1facad202ef1dc0ce3f6
Gedaias Jair <gedaias@geda.dev>
2024-02-06 20:14:08 -0600

    setup project

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./styles/App.css";
+import CurrentWeather from "./components/CurrentWeather";
+
+function App() {
+  return (
+    <>
+      <CurrentWeather />
+    </>
+  );
+}
+
+export default App;
