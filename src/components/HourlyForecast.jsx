e61a69ac37a280786f4db5278cc5a95cef0feda7
Gedaias Jair <gedaias@geda.dev>
2024-02-07 23:16:34 -0600

    Add arrows and a function to navigate through the cards

--- src/components/HourlyForecast.jsx
+++ src/components/HourlyForecast.jsx
@@ -1,10 +1,18 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import PropTypes from "prop-types";
 import HourlyCard from "./HourlyCard";
 import "../styles/hourlyForecast.css";
+import KeyboardDoubleArrowLeftIcon from "@mui/icons-material/KeyboardDoubleArrowLeft";
+import KeyboardDoubleArrowRightIcon from "@mui/icons-material/KeyboardDoubleArrowRight";
 
 export default function HourlyForecast({ city = "mexico" }) {
   const [hourlyData, setHourlyData] = useState([]);
+  const [slidePosition, setSlidePosition] = useState(0);
+  const [buttonDisabled, setButtonDisabled] = useState({
+    left: true,
+    right: false,
+  });
+  const cardsContainer = useRef(null);
 
   useEffect(() => {
     fetch(
@@ -36,18 +44,47 @@ export default function HourlyForecast({ city = "mexico" }) {
     setHourlyData(updateData);
   };
 
+  const handleSlide = (position) => {
+    slidePosition + position < 0 && slidePosition + position > -2400
+      ? setButtonDisabled({ left: false, right: false })
+      : slidePosition + position === 0
+      ? setButtonDisabled({ left: true, right: false })
+      : setButtonDisabled({ left: false, right: true });
+
+    [...cardsContainer.current.children].map((card) => {
+      card.style.transform = `translateX(${slidePosition + position}px)`;
+    });
+    setSlidePosition(slidePosition + position);
+  };
+
   return (
     <section className="hourly-forecast-section">
-      {hourlyData.map((data) => (
-        <HourlyCard
-          key={data.hour}
-          hour={data.hour}
-          condition={data.condition}
-          icon={data.icon}
-          temperature={data.temperature}
-          chanceOfRain={data.chanceOfRain}
-        />
-      ))}
+      <button
+        className="arrow-button"
+        onClick={() => handleSlide(100)}
+        disabled={buttonDisabled.left}
+      >
+        <KeyboardDoubleArrowLeftIcon className="nav-arrow" />
+      </button>
+      <div className="cards-container" ref={cardsContainer}>
+        {hourlyData.map((data) => (
+          <HourlyCard
+            key={data.hour}
+            hour={data.hour}
+            condition={data.condition}
+            icon={data.icon}
+            temperature={data.temperature}
+            chanceOfRain={data.chanceOfRain}
+          />
+        ))}
+      </div>
+      <button
+        className="arrow-button"
+        onClick={() => handleSlide(-100)}
+        disabled={buttonDisabled.right}
+      >
+        <KeyboardDoubleArrowRightIcon className="nav-arrow" />
+      </button>
     </section>
   );
 }
