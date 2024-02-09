810538b80f2927f1fae5351d03a801a118018d98
Gedaias Jair <gedaias@geda.dev>
2024-02-08 21:37:32 -0600

    Create a search box to update the city in the components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,41 @@
 import "./styles/App.css";
 import CurrentWeather from "./components/CurrentWeather";
+import HourlyForecast from "./components/HourlyForecast";
+import DailyForecast from "./components/DailyForecast";
+import SearchIcon from "@mui/icons-material/Search";
+import { useState } from "react";
 
 function App() {
+  const [search, setSearch] = useState("");
+  const [city, setCity] = useState("mexico");
+
+  const handleInput = (event) => {
+    setSearch(event.target.value);
+  };
+
+  const submitCity = () => {
+    setCity(search);
+  };
+
   return (
     <>
-      <CurrentWeather />
+      <header>
+        <div className="search-box">
+          <input
+            type="text"
+            placeholder="Search City"
+            autoComplete="off"
+            value={search}
+            onChange={handleInput}
+          />
+          <button type="button" onClick={submitCity}>
+            <SearchIcon />
+          </button>
+        </div>
+      </header>
+      <CurrentWeather city={city} />
+      <HourlyForecast city={city} />
+      <DailyForecast city={city} />
     </>
   );
 }
